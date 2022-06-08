7d94ea260332d87d1417f8540a156b916218b14e
LeeChangHwan <changhwanlee94@gmail.com>
2022-06-08 14:23:39 +0900

    add fontawesome

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app';
 import Weather from './service/weather';
+import '@fortawesome/fontawesome-free/js/all.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const weatherService = new Weather();
