45f2dcdb0ad7cd7e92927b160810abc0439a0fa6
LeeChangHwan <changhwanlee94@gmail.com>
2022-06-22 16:15:47 +0900

    add datalist

--- src/components/search /search.jsx
+++ src/components/search /search.jsx
@@ -33,11 +33,38 @@ const Search = ({ weatherService, getSearchWeather }) => {
     <section className={styles.container}>
       <form className={styles.form} onSubmit={onSubmit}>
         <input
+          list="cityList"
           className={styles.input}
           ref={inputRef}
           type="text"
           placeholder="Search"
-        ></input>
+        />
+        <datalist id="cityList" className={styles.dataList}>
+          <option value="Seoul" label="서울"></option>
+          <option value="New York" label="뉴욕"></option>
+          <option value="Chicago" label="시카고"></option>
+          <option value="London" label="런던"></option>
+          <option value="Milan" label="밀라노"></option>
+          <option value="Madrid" label="마드리드"></option>
+          <option value="Vienna" label="비엔나"></option>
+          <option value="Prague" label="프라하"></option>
+          <option value="Berlin" label="베를린"></option>
+          <option value="Tokyo" label="도쿄"></option>
+          <option value="Osaka" label="오사카"></option>
+          <option value="Hong Kong" label="홍콩"></option>
+          <option value="Shanghai" label="상하이"></option>
+          <option value="Beijing" label="베이징"></option>
+          <option value="Sydney" label="시드니"></option>
+          <option value="Melbourne" label="멜버른"></option>
+          <option value="Singapore" label="싱가포르"></option>
+          <option value="Bangkok" label="방콕"></option>
+          <option value="Manila" label="마닐라"></option>
+          <option value="Amsterdam" label="암스테르담"></option>
+          <option value="Toronto" label="토론토"></option>
+          <option value="São Paulo" label="상파울로"></option>
+          <option value="Dubai" label="두바이"></option>
+          <option value="Moscow" label="모스크바"></option>
+        </datalist>
         <button className={styles.button}>
           <i className="fa-solid fa-magnifying-glass-location"></i>
         </button>
