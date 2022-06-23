4b12f5e96878058ad67f10395617aee971c86d04
LeeChangHwan <changhwanlee94@gmail.com>
2022-06-23 21:53:26 +0900

    separate  from search component

---
+++ src/components/details/details.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from './details.module.css';
+
+const Details = ({ detailInfo }) => {
+  const { weather, tempMax, tempMin, humidity, windSpeed } = detailInfo;
+
+  return (
+    <dl className={styles.details}>
+      <div className={styles.info}>
+        <dt>Weather</dt>
+        <dd>{weather}</dd>
+      </div>
+
+      <div className={styles.info}>
+        <dt>Max/Min Temp</dt>
+        <dd>{`${tempMax}℃ / ${tempMin}℃`}</dd>
+      </div>
+
+      <div className={styles.info}>
+        <dt>Humidity</dt>
+        <dd>{`${humidity} %`}</dd>
+      </div>
+
+      <div className={styles.info}>
+        <dt>Wind Speed</dt>
+        <dd>{`${windSpeed} m/s`}</dd>
+      </div>
+    </dl>
+  );
+};
+
+export default Details;
