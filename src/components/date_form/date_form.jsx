2f09b46e18ea0e01fbe441494b32f7554cdeca92
LeeChangHwan <changhwanlee94@gmail.com>
2022-06-23 21:54:44 +0900

    separate from home component

---
+++ src/components/date_form/date_form.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import styles from './date_form.module.css';
+
+const DateForm = () => {
+  const [date, setDate] = useState({
+    month: '',
+    date: '',
+    year: '',
+  });
+
+  useEffect(() => {
+    const today = new Date();
+    const month = today.getMonth();
+    const date = String(today.getDate()).padStart(2, 0);
+    const year = today.getFullYear();
+
+    const monthArr = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+
+    setDate({
+      month: monthArr[month],
+      date: date,
+      year: year,
+    });
+  }, []);
+
+  return (
+    <div className={styles.today}>
+      <i className="fa-regular fa-calendar"></i>
+      <span className={styles.month}>{date.month}</span>
+      <span className={styles.date}>{`${date.date},`}</span>
+      <span className={styles.year}>{date.year}</span>
+    </div>
+  );
+};
+
+export default DateForm;
